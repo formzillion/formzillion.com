0f2cdf6521ae99698403590d8f679b22de6936f9
punithraj21 <punithraj.tcs21@gmail.com>
2023-05-04 15:45:10 +0530

    applogo fix

--- apps/webapp/src/ui/AppLogo.tsx
+++ apps/webapp/src/ui/AppLogo.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import clsx from "clsx";
 import Image from "next/image";
 import Logo from "../../public/logos/logo.png";
 
-export default function AppLogo() {
+export default function AppLogo({
+  className,
+}: {
+  className?: string;
+}) {
   return (
-    <Image
-      src={Logo}
-      alt="Formzillion Logo"
-      className="w-auto h-10 object-contain"
-    />
+    <div className={clsx("flex", className)}>
+      <Image
+        src={Logo}
+        alt="Formzillion Logo"
+        className="w-auto h-10 object-contain"
+      />
+    </div>
   );
 }
