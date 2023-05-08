a0a2b6265caae547e220762671bca2943fd623e5
HarshShah08 <harsh.shah@codosphere.com>
2023-05-08 14:20:40 +0530

    feat: :art: changed Billing and invoices to server component
    changed Billing and invoices to server component

--- apps/webapp/src/app/(portal)/[teamId]/team/settings/billing/Subscription.tsx
+++ apps/webapp/src/app/(portal)/[teamId]/team/settings/billing/Subscription.tsx
@@ -1,32 +1,15 @@
-"use client";
-import React, { useCallback, useEffect, useState } from "react";
+import React from "react";
 import { format } from "date-fns";
 import { FireIcon } from "@heroicons/react/24/solid";
+import stripeApi from "@/lib/stripe/stripe-api";
 
-export default function Subscription({ customerId, planId }: any) {
-  const [account, setAccount] = useState<any>([]);
-  const [loading, setLoading] = useState<any>(false);
+export default async function Subscription({ customerId, planId }: any) {
+  const subscriptions: any = await stripeApi.listSubscriptions({
+    customerId,
+    plan: planId,
+  });
+  const subscription = subscriptions.data?.[0];
 
-  const getData = useCallback(async () => {
-    setLoading(true);
-
-    fetch("/api/stripe/account", {
-      method: "POST",
-      body: JSON.stringify({
-        customerId,
-        planId,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => setAccount(data));
-    setLoading(false);
-  }, []);
-
-  useEffect(() => {
-    getData().then(() => console.log("fetched"));
-  }, []);
-
-  const subscription = account?.subscriptions?.[0];
   const plan = subscription?.plan || {};
   const { name } = plan?.product || {};
   const { amount_decimal = 0, interval = "month" } = plan || {};
@@ -34,41 +17,44 @@ export default function Subscription({ customerId, planId }: any) {
   const nextBillingDate = format(current_period_end * 1000, "dd/MMM/yyyy");
 
   return (
-    <div className="space-y-5">
-      <div className="bg-white shadow dark:bg-black border border-gray-300 dark:border-gray-700">
-        <div className="p-4 px-6 flex items-center gap-4">
-          <div className="relative rounded-full bg-orange-500 flex h-10 w-10 items-center justify-center">
-            <FireIcon className="h-5 w-5 text-white" />
-          </div>
-          <div>
-            <div className="flex items-center">
-              <h6 className="text-base font-semibold text-gray-700 dark:text-white">
-                {name} <span>Plan</span>
-              </h6>
-              <div className=" mx-2 inline-flex items-center rounded-md border-0 bg-emerald-100 px-2 py-1 text-xs font-semibold leading-4 text-emerald-600">
-                <span className="capitalize">active</span>
-              </div>
+    <>
+      <div className="space-y-5">
+        <div className="bg-white shadow dark:bg-black border border-gray-300 dark:border-gray-700">
+          <div className="p-4 px-6 flex items-center gap-4">
+            <div className="relative rounded-full bg-orange-500 flex h-10 w-10 items-center justify-center">
+              <FireIcon className="h-5 w-5 text-white" />
             </div>
-            <div className="text-sm text-gray-500">
-              <span>Billing {interval}ly</span>
-              <span> | </span>
-              <span>
-                {current_period_end === 0
-                  ? "Your don't have any current plan "
-                  : `Next payment on ${nextBillingDate}`}
-              </span>
-              <span>
-                <span className="mx-1">
-                  {current_period_end !== 0 && "for"}
+            <div>
+              <div className="flex items-center">
+                <h6 className="text-base font-semibold text-gray-700 dark:text-white">
+                  {name} <span>Plan</span>
+                </h6>
+                <div className=" mx-2 inline-flex items-center rounded-md border-0 bg-emerald-100 px-2 py-1 text-xs font-semibold leading-4 text-emerald-600">
+                  <span className="capitalize">active</span>
+                </div>
+              </div>
+              <div className="text-sm text-gray-500">
+                <span>Billing {interval}ly</span>
+                <span> | </span>
+                <span>
+                  {current_period_end === 0
+                    ? "Your don't have any current plan "
+                    : `Next payment on ${nextBillingDate}`}
                 </span>
-                <span className="font-semibold text-gray-900 dark:text-white">
-                  {current_period_end !== 0 && `USD ${amount_decimal / 100}.00`}
+                <span>
+                  <span className="mx-1">
+                    {current_period_end !== 0 && "for"}
+                  </span>
+                  <span className="font-semibold text-gray-900 dark:text-white">
+                    {current_period_end !== 0 &&
+                      `USD ${amount_decimal / 100}.00`}
+                  </span>
                 </span>
-              </span>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
--- apps/webapp/src/app/(portal)/[teamId]/team/settings/invoices/invoices.tsx
+++ apps/webapp/src/app/(portal)/[teamId]/team/settings/invoices/invoices.tsx
@@ -1,39 +1,25 @@
-"use client";
+import React from "react";
+import Stripe from "stripe";
 import BillingHistory from "@/app/(portal)/account/billing/BillingHistory";
-import React, { useCallback, useEffect, useState } from "react";
+import stripeApi from "@/lib/stripe/stripe-api";
 
 interface BillingProps {
   userDetails: any;
 }
 
-export default function Invoices(props: BillingProps) {
+export default async function Invoices(props: BillingProps) {
   const { userDetails } = props;
-  const [loading, setLoading] = useState<any>(false);
-  const [account, setAccount] = useState<any>([]);
+
   const customerId = userDetails?.billingCustomerId;
-  const { planId } = userDetails;
-  const getData = useCallback(async () => {
-    setLoading(true);
-
-    fetch("/api/stripe/account", {
-      method: "POST",
-      body: JSON.stringify({
-        customerId,
-        planId,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => setAccount(data));
-    setLoading(false);
-  }, []);
-
-  useEffect(() => {
-    getData().then(() => console.log("fetched"));
-  }, []);
+  const invoices: Stripe.ApiList<Stripe.Invoice> = await stripeApi.listInvoices(
+    {
+      customerId,
+    }
+  );
 
   return (
     <>
-      <BillingHistory account={account} loading={loading} />
+      <BillingHistory invoices={invoices.data} />
     </>
   );
 }
