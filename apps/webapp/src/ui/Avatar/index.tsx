79b5fc2ed0d93c51caa0fca937bbe8a7d468e58a
sahana-sv <sahana.hv@codosphere.com>
2023-05-09 17:16:39 +0530

    feat: :art: Changed loader background color, Fixed icon for mode toggle & Renamed to Formzillion homepage instead of Back to Formzillion

--- apps/webapp/src/ui/Avatar/index.tsx
+++ apps/webapp/src/ui/Avatar/index.tsx
@@ -39,7 +39,7 @@ const AvatarFallback = React.forwardRef<
   <AvatarPrimitive.Fallback
     ref={ref}
     className={cn(
-      "flex h-full w-full items-center justify-center rounded-full bg-muted",
+      "flex h-full w-full items-center justify-center rounded-full bg-muted bg-gray-200 dark:bg-gray-800",
       className
     )}
     {...props}
