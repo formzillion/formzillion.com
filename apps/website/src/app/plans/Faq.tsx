d96cf8b2ceca4b12ba568325ad43bd20c08041fc
HarshShah08 <harsh.shah@codosphere.com>
2023-05-11 19:16:29 +0530

    feat: :art: faq section added for plans page
    faq section added for plans page

---
+++ apps/website/src/app/plans/Faq.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Accordion from "@/ui/Accordion";
+const faqs = [
+  {
+    title: "What measures do you take to ensure the security of my data?",
+    description:
+      "We prioritize the privacy and security of your data on Formzillion, both for our valued customers and our organization. We are committed to implementing the highest standards of security measures. You can review our Security policy by visiting this link.",
+  },
+  {
+    title: "How do I go about upgrading my account to premium?",
+    description:
+      "We provide three distinct premium packages tailored to varying levels of requirements. You have the freedom to choose the package that best suits your needs, and with the convenience of your credit card, you can initiate your premium subscription.",
+  },
+  {
+    title: "Is it possible to cancel my premium plan at any time?",
+    description:
+      "Absolutely! There are no limitations when it comes to downgrading your account. You can effortlessly switch from any premium package to a free one or transition between different premium plans. It's important to note that once we receive your downgrade request, you will continue to enjoy the benefits and features of your premium package until the next billing period.",
+  },
+  {
+    title: "How is my card information stored by you?",
+    description:
+      "Our services utilize the trusted payment processing services of Stripe to ensure the secure storage of your card information and seamlessly enable payment capabilities with utmost security.",
+  },
+  {
+    title: "What happens if I surpass the monthly limit for submissions?",
+    description:
+      "Rest assured! Your submissions will be safely stored in a secure location until the beginning of the following month. However, in order to access them, it is necessary to upgrade your plan. By upgrading, you'll gain the ability to retrieve and view those valuable submissions!",
+  },
+  {
+    title:
+      "Is there a restriction on the number of websites or domains allowed?",
+    description:
+      "No, we don't restrict the number of domains or websites where form submitters can submit to each form endpoint generated on Formzillion. Therefore, you can submit through the same form endpoint from different websites without any limitations.",
+  },
+];
+export default function Faq() {
+  return (
+    <div className="max-w-4xl mx-auto py-16">
+      <h1 className="text-5xl mb-10">
+        Frequently asked questions,{" "}
+        <span className="bg-gradient-to-r from-yellow-400 via-orange-500 to-orange-600 bg-clip-text text-transparent">
+          answered.
+        </span>
+      </h1>
+      <Accordion
+        items={faqs}
+        type={"single"}
+        collapsible={true}
+        className={""}
+      />
+    </div>
+  );
+}
---
+++ apps/website/src/ui/Accordion/SAccordion.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import * as React from "react";
+import * as AccordionPrimitive from "@radix-ui/react-accordion";
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
+import { cn } from "@/utils";
+
+const Accordion = AccordionPrimitive.Root;
+
+const AccordionItem = React.forwardRef<
+  React.ElementRef<typeof AccordionPrimitive.Item>,
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
+>(({ className, ...props }, ref) => (
+  <AccordionPrimitive.Item
+    ref={ref}
+    className={cn(
+      "bg-gradient-to-b from-black to-gray-950 border border-gray-500",
+      className
+    )}
+    {...props}
+  />
+));
+AccordionItem.displayName = "AccordionItem";
+
+const AccordionTrigger = React.forwardRef<
+  React.ElementRef<typeof AccordionPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
+>(({ className, children, ...props }, ref) => (
+  <AccordionPrimitive.Header className="flex">
+    <AccordionPrimitive.Trigger
+      ref={ref}
+      className={cn(
+        "flex flex-1 items-center justify-between p-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
+        className
+      )}
+      {...props}
+    >
+      {children}
+      <ChevronDownIcon className="h-4 w-4 transition-transform duration-200" />
+    </AccordionPrimitive.Trigger>
+  </AccordionPrimitive.Header>
+));
+AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName;
+
+const AccordionContent = React.forwardRef<
+  React.ElementRef<typeof AccordionPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+  <AccordionPrimitive.Content
+    ref={ref}
+    className={cn(
+      "overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down",
+      className
+    )}
+    {...props}
+  >
+    <div className="p-4 pt-0 text-gray-400">{children}</div>
+  </AccordionPrimitive.Content>
+));
+AccordionContent.displayName = AccordionPrimitive.Content.displayName;
+
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent };
