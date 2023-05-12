707ff95f3f25faf367512c1af60c9e133b14be6f
sahana-sv <sahana.hv@codosphere.com>
2023-05-12 10:06:08 +0530

    feat: :bug: Design fix for hero section

--- apps/website/src/app/home/WaitlistHeroSection/HeroRight.tsx
+++ apps/website/src/app/home/WaitlistHeroSection/HeroRight.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function HeroRight() {
   return (
-    <div className="mx-2 sm:mx-10">
+    <div className="mx-2 sm:mx-10 flex justify-center">
       <video
         className="relative z-50 bg-black w-full border-2 border-gray-600 rounded-xl mt-8 sm:mt-20 p-10 max-w-4xl"
         autoPlay
--- apps/website/src/app/home/WaitlistHeroSection/HeroSection.tsx
+++ apps/website/src/app/home/WaitlistHeroSection/HeroSection.tsx
@@ -4,7 +4,7 @@ import HeroRight from "./HeroRight";
 
 export default function HeroSection() {
   return (
-    <div className="flex justify-center items-center w-full flex-row flex-wrap max-w-7xl mx-auto relative">
+    <div className="w-full relative">
       <BlurDotGridTop />
       <HeroLeft />
       <HeroRight />
