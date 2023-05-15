adfeb0ec78ba99f4e11ace51887f11ca0efc725c
sahana-sv <sahana.hv@codosphere.com>
2023-05-15 17:14:35 +0530

    feat: :art: Redesigned footer section & added a cta button in cta section, fixed the padding issue in navbar signup button

--- apps/website/src/app/shared/FooterV2.tsx
+++ apps/website/src/app/shared/FooterV2.tsx
@@ -1,202 +1,206 @@
-import Link from "next/link";
 import AppLogo from "@/ui/AppLogo";
 import { FiGithub, FiLinkedin, FiSlack } from "react-icons/fi";
-import BlurDotGridBottom from "../home/WaitlistHeroSection/BlurDotGridBottom";
 
-const ProductItems = [
+const footerLinks = [
   {
-    label: "Templates",
-    href: "/templates",
+    title: "Product",
+    list: [
+      {
+        label: "Documentation",
+        href: "https://docs.formzillion.com",
+        target: "_blank",
+      },
+      {
+        label: "Pricing",
+        href: "/pricing",
+        target: "_self",
+      },
+      {
+        label: "Status",
+        href: "https://formzillion.github.io/status/",
+        target: "_blank",
+      },
+    ],
   },
   {
-    label: "Documentation",
-    href: "https://docs.formzillion.com",
+    title: "Company",
+    list: [
+      {
+        label: "Blog",
+        href: "#",
+        target: "_self",
+      },
+      {
+        label: "Roadmap",
+        href: "https://github.com/orgs/formzillion/projects/1",
+        target: "_blank",
+      },
+      {
+        label: "Terms",
+        href: "/terms",
+        target: "_self",
+      },
+      {
+        label: "Privacy",
+        href: "/privacy",
+        target: "_self",
+      },
+    ],
   },
   {
-    label: "Pricing",
-    href: "/pricing",
+    title: "Platforms",
+    list: [
+      {
+        label: "Wordpress",
+        href: "/for/wordpress",
+        target: "_self",
+      },
+      {
+        label: "Gatsby",
+        href: "/for/gatsby",
+        target: "_self",
+      },
+      {
+        label: "Nextjs",
+        href: "/for/nextjs",
+        target: "_self",
+      },
+      {
+        label: "Webflow",
+        href: "/for/webflow",
+        target: "_self",
+      },
+    ],
   },
   {
-    label: "Status",
-    href: "/status",
+    title: "Features",
+    list: [
+      {
+        label: "Spam Filtering",
+        href: "https://docs.formzillion.com/features/spam-filtering",
+        target: "_blank",
+      },
+      {
+        label: "Redirects",
+        href: "https://docs.formzillion.com/features/redirects",
+        target: "_blank",
+      },
+      {
+        label: "Collaboration",
+        href: "https://docs.formzillion.com/features/collaboration",
+        target: "_blank",
+      },
+      {
+        label: "Exports",
+        href: "https://docs.formzillion.com/features/exports",
+        target: "_blank",
+      },
+      {
+        label: "Email Notifications",
+        href: "https://docs.formzillion.com/features/email-notifications",
+        target: "_blank",
+      },
+      {
+        label: "Autoresponders",
+        href: "https://docs.formzillion.com/features/autoresponders",
+        target: "_blank",
+      },
+    ],
   },
   {
-    label: "Changelog",
-    href: "/changelog",
-  },
-  {
-    label: "Security",
-    href: "/security",
-  },
-  {
-    label: "Contact Sales",
-    href: "/contact-us",
-  },
-  {
-    label: "Contact Support",
-    href: "/contact-us",
-  },
-];
-
-const CompanyItems = [
-  {
-    label: "Blog",
-    href: "https://blog.formzillion.com",
-  },
-  {
-    label: "About",
-    href: "/about-us",
-  },
-  {
-    label: "Roadmap",
-    href: "https://github.com/orgs/formzillion/projects/1",
-  },
-  {
-    label: "Terms",
-    href: "/policies/tos",
-  },
-  {
-    label: "Privacy",
-    href: "/policies/privacy",
-  },
-  {
-    label: "Announcements",
-    href: "/announcements",
-  },
-];
-
-const FeaturesItem = [
-  {
-    label: "Spam Filtering",
-    href: "/features/spam-filtering",
-  },
-];
-
-const PlatformItem = [
-  {
-    label: "Wordpress",
-    href: "/for/wordpress",
-  },
-  {
-    label: "Gatsby",
-    href: "/for/gatsby",
-  },
-  {
-    label: "Nextjs",
-    href: "/for/nextjs",
-  },
-  {
-    label: "Webflow",
-    href: "/for/webflow",
+    title: "Integrations",
+    list: [
+      {
+        label: "Slack",
+        href: "https://docs.formzillion.com/integrations/slack",
+        target: "_blank",
+      },
+      {
+        label: "Sendgrid",
+        href: "https://docs.formzillion.com/integrations/sendgrid",
+        target: "_blank",
+      },
+      {
+        label: "Webhooks",
+        href: "https://docs.formzillion.com/integrations/webhooks",
+        target: "_blank",
+      },
+      {
+        label: "Mailerlite",
+        href: "https://docs.formzillion.com/integrations/mailerlite",
+        target: "_blank",
+      },
+      {
+        label: "Airtable",
+        href: "https://docs.formzillion.com/integrations/airtable",
+        target: "_blank",
+      },
+    ],
   },
 ];
 
 const FooterIcons = [
   {
-    icon: <FiLinkedin className="text-gray-400 hover:text-blue-600" />,
+    icon: <FiLinkedin className="text-gray-400 hover:text-blue-800" />,
     url: "https://www.linkedin.com/company/formzillion/",
+    hoverColor: "border-blue-800",
   },
   {
     icon: <FiGithub className="text-gray-400 hover:text-white" />,
     url: "https://github.com/formzillion",
+    hoverColor: "border-white",
   },
   {
     icon: <FiSlack className="text-gray-400 hover:text-purple-900" />,
     url: "https://formzillion.slack.com/join/shared_invite/zt-1urntbbmb-o0d6Qzdl~GzfePoZE7JTYw",
+    hoverColor: "border-purple-800",
   },
 ];
 
 export default function Footer() {
   return (
-    <div className="relative border-t border-gray-950">
-      <BlurDotGridBottom />
-      <div className="grid grid-cols-3 mx-auto max-w-6xl mt-8 text-white px-3">
-        <div className="col-span-3 md:col-span-1">
-          <AppLogo />
-          <div className="text-sm lg:text-base mt-3 ">
-            <p className="leading-normal ">
-              1511-35 Hayden Street,
-              <br />
-              Toronto, Ontario, Canada.
-            </p>
-            <br />
-            <p className="leading-normal">
-              <a href="tel:+1 (587) 988-9633">+1 (587) 988-9633</a>
-              <br />
-              <a href="mailto:support@formzillion.com">
-                support@formzillion.com
-              </a>
-            </p>
-          </div>
+    <div className="relative border-t border-gray-800 bg-gray-900/20">
+      <div className="mx-auto max-w-7xl px-4 lg:px-0">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 mt-8 text-white">
+          {footerLinks.map((data: any, idx: number) => (
+            <div key={idx} className="mt-4 md:mt-0">
+              <p className="text-gray-300 text-base font-medium">{data.title}</p>
+              <div className="gap-2 text-sm flex flex-col mt-4 text-gray-400">
+                {data.list.map((item: any, index: number) => (
+                  <a
+                    key={index}
+                    href={item.href}
+                    target={item.target}
+                    rel="noreferrer"
+                    className="hover:text-white"
+                  >
+                    {item.label}
+                  </a>
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
-        <div className="grid grid-cols-2 sm:grid-cols-4 col-span-3 md:col-span-2 mt-8 md:mt-0 relative">
-          <div>
-            <p className="text-md font-bold leading-normal underline">
-              Product
-            </p>
-            <div className="gap-4 flex flex-col font-normal text-md leading-normal mt-3">
-              {ProductItems.map((item, index) => (
-                <Link key={index} href={item.href}>
-                  {item.label}
-                </Link>
-              ))}
-            </div>
-          </div>
-          <div>
-            <p className="text-md font-bold leading-normal underline">
-              Company
-            </p>
-            <div className="gap-4 flex flex-col font-normal text-md leading-normal mt-3">
-              {CompanyItems.map((item, index) => (
-                <Link key={index} href={item.href}>
-                  {item.label}
-                </Link>
-              ))}
-            </div>
-          </div>
-          <div>
-            <p className="text-md font-bold leading-normal underline mt-8 sm:mt-0">
-              Platforms
-            </p>
-            <div className="gap-4 flex flex-col font-normal text-md leading-normal mt-3">
-              {PlatformItem.map((item, index) => (
-                <Link key={index} href={item.href}>
-                  {item.label}
-                </Link>
-              ))}
-            </div>
-          </div>
-          <div>
-            <p className="text-md font-bold leading-normal underline mt-8 sm:mt-0">
-              Features
-            </p>
-            <div className="gap-4 flex flex-col font-normal text-md leading-normal mt-3">
-              {FeaturesItem.map((item, index) => (
-                <Link key={index} href={item.href}>
-                  {item.label}
-                </Link>
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="sm:flex justify-between w-full text-white p-3 mx-auto max-w-6xl mt-8 mb-8">
-        <div className="flex items-center space-x-2">
+        <div className="mt-6 flex items-center space-x-2">
+          <span className="mr-2">Follow us</span>
           {FooterIcons.map((item: any, idx: number) => (
             <a
               key={idx}
               href={item.url}
               target="_blank"
               rel="noreferrer"
-              className="bg-gradient-to-b from-gray-900 to-gray-950 rounded h-8 w-8 flex justify-center items-center"
+              className={`border border-gray-500 rounded-full h-8 w-8 flex justify-center items-center hover:${item?.hoverColor}`}
             >
               {item.icon}
             </a>
           ))}
         </div>
-        <p className="text-md leading-normal text-center md:text-end">
-          Copyright © 2023 Zillionstack Inc. All rights reserved.
-        </p>
+        <div className="flex flex-col items-center space-y-2 sm:flex-row justify-center sm:justify-between w-full text-white py-3 mt-4 lg:mt-8 mb-8">
+          <AppLogo />
+          <p className="text-gray-300 leading-normal md:text-end text-sm sm::text-base">
+            Copyright © 2023 Zillionstack Inc. All rights reserved.
+          </p>
+        </div>
       </div>
     </div>
   );
