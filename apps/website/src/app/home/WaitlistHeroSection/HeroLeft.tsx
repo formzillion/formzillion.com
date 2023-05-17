5f91dff5ab960c82635d560502180ad396e774e7
sahana-sv <sahana.hv@codosphere.com>
2023-05-17 19:12:22 +0530

    feat: :bug: Added correct link in footer for docs & changed the Hero section text alignment

--- apps/website/src/app/home/WaitlistHeroSection/HeroLeft.tsx
+++ apps/website/src/app/home/WaitlistHeroSection/HeroLeft.tsx
@@ -16,11 +16,11 @@ export default function HeroLeft() {
   return (
     <div className="flex relative justify-center items-center text-white text-center w-full h-full mx-auto max-w-6xl">
       <div className="mt-10 flex flex-col items-center space-y-4">
-        <h1 className="font-normal text-2xl sm:text-3xl lg:text-5xl max-w-4xl">
+        <h1 className="font-normal text-2xl sm:text-3xl lg:text-5xl max-w-6xl">
           The open-source{" "}
           <span className="bg-gradient-to-r from-yellow-400 via-orange-500 to-orange-600 bg-clip-text text-transparent">
             form infrastructure
-          </span>{" "} <br/>
+          </span>{" "}
           for everyone
         </h1>
         <p className="text-base lg:text-xl leading-normal px-12 sm:max-w-3xl font-light text-gray-300">
@@ -29,7 +29,7 @@ export default function HeroLeft() {
         </p>
         <div>
           <Link
-            href={`${process.env.NEXT_PUBLIC_APP_URL}/register`}
+            href={`${process.env.NEXT_PUBLIC_APP_URL}/login`}
             className="mt-0 flex h-[44px] w-fit items-center justify-center rounded bg-orange-600 py-2.5 px-4 text-white hover:bg-orange-700"
           >
             {`Try Formzillion - It's free!`}
