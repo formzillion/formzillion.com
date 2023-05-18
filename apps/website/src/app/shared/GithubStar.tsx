4be58216afb8a6688e2264fa39cf6001cc625a1e
sahana-sv <sahana.hv@codosphere.com>
2023-05-18 18:55:29 +0530

    feat: :sparkles: Alignment changes done for github stars & Added a resources dropdown in navbar

--- apps/website/src/app/shared/GithubStar.tsx
+++ apps/website/src/app/shared/GithubStar.tsx
@@ -5,8 +5,6 @@ import Loader from "@/ui/Loader";
 
 export default function GithubStar() {
   const [starCount, setStarCount] = useState(null);
-  const [starBox, setStarBox] = useState(true);
-
   const url = "https://api.github.com/repos/formzillion/formzillion.com";
 
   useEffect(() => {
@@ -23,40 +21,28 @@ export default function GithubStar() {
     fetchStarCount();
   }, []);
 
-  const handleStar = () => {
-    setStarBox(!starBox);
-  };
-
   return (
-    <div
-      className={`border border-gray-800 rounded hidden lg:flex space-x-6 py-1 px-6 items-center text-sm ${
-        starBox === false && "lg:hidden"
-      }`}
-    >
-      <p>Star us on GitHub</p>
-      <div className="flex items-center">
-        <a
-          href="https://github.com/formzillion/formzillion.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <p className="border border-gray-900 text-white p-1 px-2 rounded-l flex gap-1 items-center bg-gray-950 hover:border-gray-500">
-            <StarIcon className="h-5 w-5 inline" />
-            Stars
-          </p>
-        </a>
-        <a
-          href="https://github.com/formzillion/formzillion.com/stargazers"
-          target="_blank"
-          rel="noreferrer"
-          className="flex items-center"
-        >
-          <p className="text-center hover:text-orange-400 p-1 px-2 rounded-r text-white border border-gray-900 border-l-0">
-            {starCount === null ? <Loader className="w-[15px]" /> : starCount}
-          </p>
-        </a>
-      </div>
-      <XMarkIcon className="h-4 w-4 inline" onClick={handleStar} />
+    <div className="flex items-center">
+      <a
+        href="https://github.com/formzillion/formzillion.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <p className="border border-gray-900 text-white p-1 px-2 rounded-l flex gap-1 items-center bg-gray-950 hover:border-gray-500">
+          <StarIcon className="h-5 w-5 inline" />
+          Stars
+        </p>
+      </a>
+      <a
+        href="https://github.com/formzillion/formzillion.com/stargazers"
+        target="_blank"
+        rel="noreferrer"
+        className="flex items-center"
+      >
+        <p className="text-center hover:text-orange-400 p-1 px-2 rounded-r text-white border border-gray-900 border-l-0">
+          {starCount === null ? <Loader className="w-[15px]" /> : starCount}
+        </p>
+      </a>
     </div>
   );
 }
