72b725873de941218bda70fd1ce3f71346c66ed4
punithraj21 <punithraj.tcs21@gmail.com>
2023-05-18 12:22:53 +0530

    Updated middleware

--- apps/webapp/src/middleware.ts
+++ apps/webapp/src/middleware.ts
@@ -17,26 +17,9 @@ export async function middleware(req: any) {
     data: { session },
   } = await supabase.auth.getSession();
 
-  const appUrls = [
-    "/dashboard",
-    "/templates",
-    "/forms",
-    "/apps",
-    "/workflows",
-    "/teams",
-    "/account",
-    "/account/billing",
-    "/account/password",
-  ];
   const preloginUrls = ["/login", "/register", "/forgot-password"];
 
   if (!session && !preloginUrls.includes(req.nextUrl.pathname)) {
-    const redirectUrl = req.nextUrl.clone();
-    redirectUrl.pathname = "/login";
-    redirectUrl.searchParams.set(`redirectedFrom`, req.nextUrl.pathname);
-    return NextResponse.redirect(redirectUrl);
-  } else if (!session && appUrls.includes(req.nextUrl.pathname)) {
-    // Auth condition not met, redirect to home page.
     const redirectUrl = req.nextUrl.clone();
     redirectUrl.pathname = "/login";
     redirectUrl.searchParams.set(`redirectedFrom`, req.nextUrl.pathname);
