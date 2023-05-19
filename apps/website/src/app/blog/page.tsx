330ae646076f874d08d13ad98d9243b30dc662a9
sahana-sv <sahana.hv@codosphere.com>
2023-05-19 19:17:08 +0530

    feat: :art: Design created for blog page

--- apps/website/src/app/blog/page.tsx
+++ apps/website/src/app/blog/page.tsx
@@ -1,64 +1,28 @@
-import { posts } from "./constants";
+import AppIcon from "@/ui/AppIcon";
+import BlogPreview from "./components/BlogPreview";
+import { getBlogMetadata } from "./components/getData";
 
 export default function Blog() {
+  const postMetadata = getBlogMetadata();
+  const postPreviews = postMetadata.map((post) => (
+    <BlogPreview key={post.slug} {...post} />
+  ));
+
   return (
     <>
-      <div className="bg-white py-8 sm:py-16">
+      <div className="py-8 sm:py-10">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto max-w-2xl lg:mx-0">
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              From the blog
+          <div className="text-center w-full flex flex-col space-y-6 items-center">
+            <h2 className="text-3xl font-bold tracking-tight flex items-center gap-2 sm:text-4xl">
+              <AppIcon src={"/logos/favicon.svg"} /> The Formzillion blog
             </h2>
-            <p className="mt-2 text-lg leading-8 text-gray-600">
-              Learn how to grow your business with our expert advice.
+            <p className="mt-2 text-lg leading-8 lg:max-w-2xl">
+              Explore the latest updates on Formzillion, in-depth tutorials on
+              building powerful forms for everyone.
             </p>
           </div>
-          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-y-16 gap-x-8 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-            {posts.map((post) => (
-              <article
-                key={post.id}
-                className="flex max-w-xl flex-col items-start justify-between"
-              >
-                <div className="flex items-center gap-x-4 text-xs">
-                  <time dateTime={post.datetime} className="text-gray-500">
-                    {post.date}
-                  </time>
-                  <a
-                    href={post.category.href}
-                    className="relative z-10 rounded-full bg-orange-200 py-1.5 px-3 font-medium text-gray-600 hover:bg-gray-100"
-                  >
-                    {post.category.title}
-                  </a>
-                </div>
-                <div className="group relative">
-                  <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                    <a href={post.href}>
-                      <span className="absolute inset-0" />
-                      {post.title}
-                    </a>
-                  </h3>
-                  <p className="mt-5 text-sm leading-6 text-gray-600 line-clamp-3">
-                    {post.description}
-                  </p>
-                </div>
-                <div className="relative mt-8 flex items-center gap-x-4">
-                  <img
-                    src={post.author.imageUrl}
-                    alt=""
-                    className="h-10 w-10 rounded-full bg-gray-50"
-                  />
-                  <div className="text-sm leading-6">
-                    <p className="font-semibold text-gray-900">
-                      <a href={post.author.href}>
-                        <span className="absolute inset-0" />
-                        {post.author.name}
-                      </a>
-                    </p>
-                    <p className="text-gray-600">{post.author.role}</p>
-                  </div>
-                </div>
-              </article>
-            ))}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:space-x-6 mt-8">
+            {postPreviews}
           </div>
         </div>
       </div>
