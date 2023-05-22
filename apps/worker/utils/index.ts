4f4582d96ea53fe8209cca0ed3d29879e80213b4
Abdul Wadudh Samdani <abdulwadudh5@gmail.com>
2023-05-22 11:56:01 +0530

    feat: :zap: Replaced Knex with Prisma Client in Worker

--- apps/worker/utils/index.ts
+++ apps/worker/utils/index.ts
@@ -1,3 +1,4 @@
 export { default as httpClient } from "./http-client";
 export { default as Logger } from "./logger";
 export { default as bullMq } from "./bull-mq";
+export { default as prisma } from "./prisma";
