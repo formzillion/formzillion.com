5f13daa51ca206e859e6ad32297c6269cc605ec9
Abdul Wadudh Samdani <abdulwadudh5@gmail.com>
2023-05-22 20:16:23 +0530

    Modified the database package

--- packages/database/tsup.config.ts
+++ packages/database/tsup.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   clean: true,
   dts: true,
-  entry: ["src/index.ts"],
+  entry: ["index.ts"],
   format: ["cjs", "esm"],
   minify: true,
   sourcemap: true,
