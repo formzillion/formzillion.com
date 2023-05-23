c88ac3a19c54b04c27cddfff69a86270ded1f884
sahana-sv <sahana.hv@codosphere.com>
2023-05-23 18:54:45 +0530

    feat: :art: Design fix done for banner section & added content & video for banner section in all pages
    Added a steps in how to create section for slack page

--- apps/website/src/app/integrations/[slug]/page.tsx
+++ apps/website/src/app/integrations/[slug]/page.tsx
@@ -1,83 +1,74 @@
 import React from "react";
-import Image from "next/image";
 import Link from "next/link";
-import {
-  BoltIcon,
-  CursorArrowRaysIcon,
-  PlusCircleIcon,
-  Square3Stack3DIcon,
-} from "@heroicons/react/24/outline";
+import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 
 import HowToConnect from "./HowToConnect";
+import AppsLogo from "./AppsLogo";
+import { heroSection } from "./content/heroSection";
 
-export default function page() {
+export default function page({ params }: { params: { slug: string } }) {
+  const slug = params?.slug;
+  const pageContent = heroSection.find((content: any) => content.slug === slug);
+
   return (
-    <div className="container max-w-5xl mt-16">
-      <div className="rounded bg-gray-900/50 py-6 px-10">
-        <div className="flex items-center justify-center my-4 space-x-4">
-          <div className="h-16 w-16 bg-white rounded-full p-2 flex items-center justify-center">
-            <Image
-              src="/favicon.svg"
-              alt="logo"
-              height={40}
-              width={40}
-              className="object-contain w-15 w-15"
-            />
-          </div>
-          <PlusCircleIcon className="inline w-8 h-8 mr-1" />
-          <div className="h-16 w-16 bg-white rounded-full p-2">
-            <Image
-              src="/brands/slack.png"
-              alt="app-logo"
-              height={80}
-              width={80}
-              className="object-contain w-full h-full"
-            />
-          </div>
-        </div>
-        <div className="text-center">
-          <h4 className="text-2xl sm:text-3xl lg:text-5xl font-normal bg-gradient-to-r from-yellow-400 via-orange-600 to-orange-800 bg-clip-text text-transparent">
-            Slack
-          </h4>
-          <p className="text-lg mt-4">
-            Seamlessly integrate Slack with your daily apps on Formzillion,
-            empowering team collaboration for faster goal achievement. With zero
-            code integration, automate message notifications to specific
-            channels, enabling your sales team to close deals more efficiently.
-          </p>
-        </div>
-        <div className="mt-12 grid grid-cols-2 items-center justify-center relative">
-          <div className="space-y-6">
-            <p className="flex">
-              <Square3Stack3DIcon className="inline w-8 h-8 mr-1 text-orange-500" />
-              Stay structured — Publish fresh form submissions to designated
-              public or private Slack channels.
-            </p>
-
-            <p className="flex">
-              <BoltIcon className="inline w-8 h-8 mr-1 text-orange-500" />
-              Real-time notifications — Gain instant access to form submission
-              data, enabling your team to respond swiftly.
-            </p>
-
-            <p className="flex">
-              <CursorArrowRaysIcon className="inline w-8 h-8 mr-1 text-orange-500" />
-              Easy configuration — Establish a direct connection to your Slack
-              workspace with just a few clicks.
-            </p>
-          </div>
-          <div className="flex flex-col items-center space-y-2">
-            <Link
-              href={`${process.env.NEXT_PUBLIC_APP_URL}/register`}
-              className="mt-0 flex items-center justify-center rounded bg-orange-600 py-2 px-4 text-white hover:bg-orange-700"
-            >
-              Get Started for free
-            </Link>
-            <p>No Credit card required</p>
-          </div>
-        </div>
+    <div className="container max-w-5xl mt-10">
+      <AppsLogo pageContent={pageContent} />
+      <div className="text-center">
+        <h4 className="text-2xl sm:text-3xl lg:text-5xl font-normal">
+          Formzillion with{" "}
+          <span className="bg-gradient-to-r from-yellow-400 via-orange-500 to-orange-600 bg-clip-text text-transparent">
+            {pageContent?.title}{" "}
+          </span>
+        </h4>
+        <p className="text-lg mt-4 text-gray-400">{pageContent?.description}</p>
       </div>
-      <HowToConnect />
+      <div className="flex justify-center space-x-4 items-center mt-4">
+        <Link
+          href={`${process.env.NEXT_PUBLIC_APP_URL}/register`}
+          className="mt-0 flex items-center justify-center rounded bg-orange-600 py-2 px-4 text-white hover:bg-orange-700"
+        >
+          Get Started for free
+        </Link>
+        <Link
+          href={pageContent?.docsLink}
+          className="mt-0 flex items-center justify-center rounded border border-gray-400 py-2 px-4 hover:text-orange-600"
+        >
+          Learn more
+        </Link>
+      </div>
+
+      <div className="grid grid-cols-2 gap-4 mt-12 items-center">
+        <div className="space-y-6">
+          {pageContent?.steps.map((step: any) => (
+            <div className="flex items-center space-x-2 ">
+              {step.icon}
+              <p className="w-[90%] text-gray-300">{step.description}</p>
+            </div>
+          ))}
+        </div>
+        <video
+          className="relative z-50 bg-black w-full border-2 border-gray-600 rounded-xl p-3 max-w-4xl"
+          autoPlay
+          muted
+          loop
+          playsInline
+        >
+          <source
+            src={`/integrations/${pageContent?.videoUrl}`}
+            type="video/mp4"
+          />
+        </video>
+      </div>
+      <div className="flex justify-center items-center mt-8">
+        <Link href="/integrations">
+          <p className="text-base text-orange-500 hover:text-orange-600">
+            <ArrowLeftIcon className="inline w-5 h-4 mr-1" />
+            All Integrations
+          </p>
+        </Link>
+      </div>
+      <div className="border-b border-gray-800 border-dashed my-10"></div>
+      <HowToConnect slug={slug} />
     </div>
   );
 }
