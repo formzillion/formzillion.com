db6ddfc469626edb31eef84e9bb4ca31b7aae5a0
HarshShah08 <harsh.shah@codosphere.com>
2023-05-25 12:43:55 +0530

    fix: :bug: docs link added to featueres section and typo fix done forr compare page
    docs link added to featueres section and typo fix done forr compare page

--- apps/website/src/app/home/FeaturesOverviewNew/index.tsx
+++ apps/website/src/app/home/FeaturesOverviewNew/index.tsx
@@ -5,27 +5,32 @@ import {
   ListBulletIcon,
 } from "@heroicons/react/24/solid";
 import ThirdPartyApps from "./ThirdPartyApps";
+import Link from "next/link";
 
 const fertures = [
   {
     title: "Data validated, spam blocked.",
+    docsUrl: `${process.env.NEXT_PUBLIC_DOCS_URL}/features/spam-filtering`,
     subtitle:
       "Your data undergoes server-side validation, while our advanced machine learning algorithms work tirelessly to shield you from spam.",
     icon: <ShieldCheckIcon className="w-8 text-green-500" />,
   },
   {
     title: "Email notifications and automatic responses are dispatched.",
+    docsUrl: `${process.env.NEXT_PUBLIC_DOCS_URL}/features/email-notifications`,
     subtitle:
       "Responsiveness through automated email notifications and auto-responses",
     icon: <EnvelopeIcon className="w-8 text-yellow-500" />,
   },
   {
     title: "Submissions are saved to your Formzillion account.",
+    docsUrl: `${process.env.NEXT_PUBLIC_DOCS_URL}/features/exports`,
     subtitle: "View submissions and export them as CSV or JSON files.",
     icon: <ListBulletIcon className="w-8" />,
   },
   {
     title: "Third party integrations come into action.",
+    docsUrl: `${process.env.NEXT_PUBLIC_DOCS_URL}/integrations`,
     subtitle:
       "We eliminate the need for Zapier like application by utilizing our direct integrations to effortlessly transport your data to its intended destination.",
     icon: <ShareIcon className="w-8 text-blue-500" />,
@@ -44,9 +49,10 @@ const FeaturesOverviewNew = () => {
       <div className=" lg:col-span-3 mt-5">
         <div className=" space-y-5">
           {fertures.map((ferture, idx) => (
-            <div
+            <Link
+              href={ferture.docsUrl}
               key={idx}
-              className="p-2 border rounded border-gray-900 space-y-4 hover-border"
+              className="block p-2 border rounded border-gray-900 space-y-4 hover-border"
             >
               <div className="bg-black p-4 rounded relative z-10 h-full space-y-5">
                 <div className=" flex space-x-5">
@@ -60,7 +66,7 @@ const FeaturesOverviewNew = () => {
                 </div>
                 {idx === 3 ? <ThirdPartyApps /> : ""}
               </div>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
