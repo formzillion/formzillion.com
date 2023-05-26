5e481caf98ea6cc51948606efb7f2bcc231d6c28
punithraj21 <punithraj.tcs21@gmail.com>
2023-05-26 18:11:32 +0530

    Showing team Name instead of my form

--- apps/webapp/src/app/(portal)/[teamId]/layout.tsx
+++ apps/webapp/src/app/(portal)/[teamId]/layout.tsx
@@ -23,7 +23,8 @@ export default async function TeamLayout({ children, params }: any) {
     notFound();
   }
 
-  const team = user?.teams?.filter((team: any) => team.slug === finalTeamId);
+  const team = user?.teams?.find((team: any) => team.slug === finalTeamId);
+
   const teamType = get(team, "0.type", "");
   const tabs = [
     { name: "Forms", path: `/`, targetSegment: null },
@@ -36,7 +37,7 @@ export default async function TeamLayout({ children, params }: any) {
 
   return (
     <div className="mx-auto max-w-7xl">
-      <PrimaryNav tabs={tabs} finalTeamId={finalTeamId} formId={formId} />
+      <PrimaryNav tabs={tabs} team={team} formId={formId} />
       {children}
     </div>
   );
