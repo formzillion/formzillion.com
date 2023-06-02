775c6537fcbbfcf1c62dc062f0715bbdba3d6c41
punithraj21 <punithraj.tcs21@gmail.com>
2023-06-02 18:24:14 +0530

    feat: :sparkles: Added Analytics Apis

--- apps/webapp/src/pages/api/form-submission/summary.ts
+++ apps/webapp/src/pages/api/form-submission/summary.ts
@@ -12,25 +12,63 @@ export default async function handler(
     formId,
     groupBy = "createdAt",
   } = JSON.parse(req.body);
-  let groupId: any = {
-    $dateToString: { format: "%Y-%m-%d", date: "$createdAt" },
-  };
 
+  let summaryData: any;
   if (type === "daily") {
-    groupId = {
-      $dateToString: { format: "%Y-%m-%d", date: "$createdAt" },
-    };
+    summaryData = await prisma.form_submissions.groupBy({
+      by: ["formId"],
+      where: {
+        createdAt: {
+          gte: new Date(new Date().setDate(new Date().getDate() - 1)),
+          lte: new Date(),
+        },
+        formId,
+      },
+      _count: {
+        formId: true,
+      },
+    });
   } else if (type === "monthly") {
-    groupId = {
-      $dateToString: { format: "%Y-%m", date: "$createdAt" },
-    };
+    summaryData = await prisma.form_submissions.groupBy({
+      by: ["formId"],
+      where: {
+        createdAt: {
+          gte: new Date(new Date().setDate(new Date().getDate() - 30)),
+          lte: new Date(),
+        },
+        formId,
+      },
+      _count: {
+        formId: true,
+      },
+    });
+  } else if (type === "weekly") {
+    summaryData = await prisma.form_submissions.groupBy({
+      by: ["formId"],
+      where: {
+        createdAt: {
+          gte: new Date(new Date().setDate(new Date().getDate() - 7)),
+          lte: new Date(),
+        },
+        formId,
+      },
+      _count: {
+        formId: true,
+      },
+    });
   }
 
   if (groupBy === "country") {
-    groupId = "$country";
+    summaryData = await prisma.form_submissions.groupBy({
+      by: [groupBy],
+      where: {
+        formId,
+      },
+      _count: {
+        formId: true,
+      },
+    });
   }
-
-  const summaryData: any = [];
 
   return res.status(201).json({ success: true, data: summaryData });
 }
