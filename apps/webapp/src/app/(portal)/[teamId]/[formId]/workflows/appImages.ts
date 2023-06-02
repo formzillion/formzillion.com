f9b4c87fa256fff9eefe9573725bbbfe13d69bdd
Abdul Wadudh Samdani <abdulwadudh5@gmail.com>
2023-06-02 11:37:02 +0530

    Removed the Unsued apps and workflows

--- apps/webapp/src/app/(portal)/[teamId]/[formId]/workflows/appImages.ts
+++
@@ -1,9 +0,0 @@
-export { default as formzillion } from "public/logos/logo_icon.png";
-export { default as sendgrid } from "public/brands/sendgrid.png";
-export { default as slack } from "public/brands/slack.png";
-export { default as mailchimp } from "public/brands/mailchimp.png";
-export { default as mailerlite } from "public/brands/mailerlite.png";
-export { default as webhooks } from "public/brands/webhooks.png";
-export { default as freshdesk } from "public/brands/freshdesk.png";
-export { default as airtable } from "public/brands/airtable.png";
-export { default as convertkit } from "public/brands/convertkit.png";
